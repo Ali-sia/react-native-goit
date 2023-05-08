155b478dc5062f7cff8cdf8cf5d89013b841b3e3
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-08 20:31:58 +0300

    add  fonts

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    robotoBold: require('./assets/fonts/Roboto-Bold.ttf'),
+    robotoMedium: require('./assets/fonts/Roboto-Medium.ttf'),
+    robotoRegular: require('./assets/fonts/Roboto-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground
+        source={require('./assets/images/RegisterLoginBG.png')}
+        style={styles.background}
+      >
+        <Text style={styles.textStyle}>
+          Open up App.js to start working on your app!
+        </Text>
+        <Text style={styles.textStyle}>^-^</Text>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  textStyle: {
+    fontSize: 30,
+    color: '#ffffff',
+    fontFamily: 'robotoBold',
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
