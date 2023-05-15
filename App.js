3d59a2af031d16a5e7c70a18e40a75b7b19f1245
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-15 15:02:20 +0300

    exporting the function to a separate file

--- App.js
+++ App.js
@@ -1,93 +1,13 @@
 // import { useState } from 'react';
 import 'react-native-gesture-handler';
 
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  ImageBackground,
-  TouchableWithoutFeedback,
-  Keyboard,
-  Image,
-} from 'react-native';
-
 import { useFonts } from 'expo-font';
-
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import Register from './src/screens/auth/RegistrationScreen';
-import Login from './src/screens/auth/LoginScreen';
-import Home from './src/screens/mainScreen/HomeScreen';
-import Profile from './src/screens/mainScreen/ProfileScreen';
-import Create from './src/screens/mainScreen/CreateScreen';
-
-const AuthStack = createStackNavigator();
-const MainTab = createBottomTabNavigator();
-
-function useAuth(isAuth) {
-  if (!isAuth) {
-    return (
-      <AuthStack.Navigator initialRouteName="LoginScreen">
-        <AuthStack.Screen
-          options={{ headerShown: false }}
-          name="LoginScreen"
-          component={Login}
-        />
-        <AuthStack.Screen
-          options={{ headerShown: false }}
-          name="RegistrationScreen"
-          component={Register}
-        />
-      </AuthStack.Navigator>
-    );
-  }
-  return (
-    <MainTab.Navigator>
-      <MainTab.Screen
-        name="HomeScreen"
-        component={Home}
-        options={({ navigation }) => ({
-          title: 'Публікації',
-          headerStyle: {
-            backgroundColor: '#ffffff',
-            shadowColor: '#000',
-            shadowOffset: { width: 0, height: 0.5 },
-            shadowOpacity: 0.3,
-            shadowRadius: 0,
-            elevation: 1,
-          },
-          headerTintColor: '#212121',
-          headerTitleStyle: {
-            fontFamily: 'robotoMedium',
-            fontSize: 17,
-            lineHeight: 22,
-          },
-          headerTitleAlign: 'center',
-          headerLeft: () => <View style={{ width: 0 }}></View>,
-          headerRight: () => (
-            <TouchableOpacity
-              style={styles.logoutIcon}
-              activeOpacity={0.8}
-              onPress={() => navigation.navigate('LoginScreen')}
-            >
-              <Image
-                source={require('./assets/images/icons/logout.png')}
-              ></Image>
-            </TouchableOpacity>
-          ),
-        })}
-      />
-      <MainTab.Screen name="CreateScreen" component={Create} />
-      <MainTab.Screen name="ProfileScreen" component={Profile} />
-    </MainTab.Navigator>
-  );
-}
+import useRoute from './router';
 
 export default function App() {
-  const routing = useAuth({});
+  const routing = useRoute({});
 
   const [fontsLoaded] = useFonts({
     robotoBold: require('./assets/fonts/Roboto-Bold.ttf'),
@@ -100,62 +20,3 @@ export default function App() {
 
   return <NavigationContainer>{routing}</NavigationContainer>;
 }
-
-{
-  /* <AuthStack.Navigator initialRouteName="LoginScreen">
-        <AuthStack.Screen
-          options={{ headerShown: false }}
-          name="LoginScreen"
-          component={Login}
-        />
-        <AuthStack.Screen
-          options={{ headerShown: false }}
-          name="RegistrationScreen"
-          component={Register}
-        /> */
-}
-{
-  /* <AuthStack.Screen
-          name="HomeScreen"
-          component={Home}
-          options={({ navigation }) => ({
-            title: 'Публікації',
-            headerStyle: {
-              backgroundColor: '#ffffff',
-              shadowColor: '#000',
-              shadowOffset: { width: 0, height: 0.5 },
-              shadowOpacity: 0.3,
-              shadowRadius: 0,
-              elevation: 1,
-            },
-            headerTintColor: '#212121',
-            headerTitleStyle: {
-              fontFamily: 'robotoMedium',
-              fontSize: 17,
-              lineHeight: 22,
-            },
-            headerTitleAlign: 'center',
-            headerLeft: () => <View style={{ width: 0 }}></View>,
-            headerRight: () => (
-              <TouchableOpacity
-                style={styles.logoutIcon}
-                activeOpacity={0.8}
-                onPress={() => navigation.navigate('LoginScreen')}
-              >
-                <Image
-                  source={require('./assets/images/icons/logout.png')}
-                ></Image>
-              </TouchableOpacity>
-            ),
-          })}
-        /> */
-}
-{
-  /* </AuthStack.Navigator> */
-}
-
-const styles = StyleSheet.create({
-  logoutIcon: {
-    marginRight: 16,
-  },
-});
