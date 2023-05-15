9bb1479ffadd9425c5f5ee56cbdfc243d58c2e1c
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-15 12:24:05 +0300

    create Post component

---
+++ src/components/Post.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Image, Text, TouchableOpacity } from 'react-native';
+
+import locationIcon from '../../assets/images/icons/location.png';
+import commentsIcon from '../../assets/images/icons/message.png';
+import commentsFullIcon from '../../assets/images/icons/messageFull.png';
+import likesIcon from '../../assets/images/icons/thumbsUp.png';
+import likesFullIcon from '../../assets/images/icons/thumbsUpFull.png';
+
+export default function Post({
+  postImg,
+  postName,
+  comments,
+  likes,
+  postLocation,
+}) {
+  return (
+    <View
+      style={styles.post}
+      //   onPress={() => {
+      //     alert('crete new POST');
+      //   }}
+      //   activeOpacity={0.8}
+    >
+      <Image source={postImg} style={styles.image}></Image>
+      <Text style={styles.title}>{postName}</Text>
+      <View style={styles.additionContainer}>
+        <View style={styles.reactionHolder}>
+          <View style={styles.commentsHolder}>
+            <Image
+              style={styles.commentsIcon}
+              source={comments.length === 0 ? commentsIcon : commentsFullIcon}
+            ></Image>
+            <Text style={comments.length === 0 && styles.commentsCount}>
+              {comments.length}
+            </Text>
+          </View>
+          <View style={styles.likesHolder}>
+            <Image
+              style={styles.likesIcon}
+              source={likes === 0 ? likesIcon : likesFullIcon}
+            ></Image>
+            <Text style={likes === 0 && styles.likesCount}>{likes}</Text>
+          </View>
+        </View>
+        <View style={styles.locationHolder}>
+          <Image style={styles.locationIcon} source={locationIcon}></Image>
+          <Text style={styles.locationText}>{postLocation}</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
