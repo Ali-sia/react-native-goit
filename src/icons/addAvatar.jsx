96cdb594c6f8fc094f9aee9135cd838a4b0eafc2
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-16 11:40:08 +0300

    create icon for register form

---
+++ src/icons/addAvatar.jsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Svg, { Circle, Path } from 'react-native-svg';
+import { memo } from 'react';
+
+function svgComponent(props) {
+  return (
+    <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={25}
+      height={25}
+      fill="none"
+      {...props}
+    >
+      <Circle
+        cx={13.5}
+        cy={13.5}
+        r={12}
+        fill="#fff"
+        // stroke="#E8E8E8"
+        transform="rotate(-.726 13.5 13.5)"
+      />
+      <Path
+        // fill="#BDBDBD"
+        fillRule="evenodd"
+        d="m13.917 6.994-1 .013.076 6-5.999.075.013 1 6-.076.075 6 1-.013-.076-6 6-.075-.013-1-6 .076-.076-6Z"
+        clipRule="evenodd"
+      />
+    </Svg>
+  );
+}
+const AddAvatarIcon = memo(svgComponent);
+export default AddAvatarIcon;
