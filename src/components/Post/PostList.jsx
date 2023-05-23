92bc5455f329ed87d9b2f7faf34a462a9cc359b1
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-23 10:26:31 +0300

    update postList to get real data

--- src/components/Post/PostList.jsx
+++ src/components/Post/PostList.jsx
@@ -13,46 +13,47 @@ const commentsFake = [
   { author: 'rrrr', text: '!!!!! incredible!!!!!!!!!!!' },
 ];
 const likesFake = 0;
-const postsFake = [
-  {
-    postId: '1',
-    postImg: postExample1,
-    postName: 'Ліс',
-    comments: commentsFake,
-    likes: likesFake,
-    postLocation: 'Ukraine',
-  },
-  {
-    postId: '2',
-    postImg: postExample2,
-    postName: 'Лxfgxfvxvxіс',
-    comments: commentsFake,
-    likes: likesFake,
-    postLocation: 'Ukraine',
-  },
-  {
-    postId: '3',
-    postImg: postExample3,
-    postName: 'fffffffffffffffffff',
-    comments: commentsFake,
-    likes: likesFake,
-    postLocation: 'Italy',
-  },
-];
+// const postsFake = [
+//   {
+//     postId: '1',
+//     postImg: postExample1,
+//     postName: 'Ліс',
+//     comments: commentsFake,
+//     likes: likesFake,
+//     postLocation: 'Ukraine',
+//   },
+//   {
+//     postId: '2',
+//     postImg: postExample2,
+//     postName: 'Лxfgxfvxvxіс',
+//     comments: commentsFake,
+//     likes: likesFake,
+//     postLocation: 'Ukraine',
+//   },
+//   {
+//     postId: '3',
+//     postImg: postExample3,
+//     postName: 'fffffffffffffffffff',
+//     comments: commentsFake,
+//     likes: likesFake,
+//     postLocation: 'Italy',
+//   },
+// ];
 
-export default function PostList() {
+export default function PostList({ posts }) {
   return (
     <View style={styles.container}>
       <SafeAreaView style={styles.safeArea}>
         <FlatList
-          data={postsFake}
+          data={posts}
           renderItem={({ item }) => (
             <Post
-              postImg={item.postImg}
-              postName={item.postName}
-              comments={item.comments}
-              likes={item.likes}
-              postLocation={item.postLocation}
+              post={item}
+              // postImg={item.postImg}
+              // postName={item.postName}
+              // comments={item.comments}
+              // likes={item.likes}
+              // postLocation={item.postLocation}
             />
           )}
           keyExtractor={item => item.postId}
