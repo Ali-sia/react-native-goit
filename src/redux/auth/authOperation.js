2749bbd7fb454d4053c431485deb762eaa275743
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-30 10:31:40 +0300

    create login redux operation

--- src/redux/auth/authOperation.js
+++ src/redux/auth/authOperation.js
@@ -1,20 +1,36 @@
-import db from '../../firebase/config';
+import { db } from '../../firebase/config';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  onAuthStateChanged,
+  signOut,
+} from 'firebase/auth';
 
+import { auth } from '../../firebase/config';
 export const authSignUpUser =
   ({ login, email, password }) =>
-  async (dispatch, getState) => {
-    console.log('login, email, password', login, email, password);
+  async dispatch => {
     try {
-      const user = await db
-        .auth()
-        .createUserWithEmailAndPassword(email, password);
-      console.log('user', user);
+      await createUserWithEmailAndPassword(auth, email, password);
+      const userSuccess = auth.currentUser;
+      console.log('userSuccess::', userSuccess);
     } catch (error) {
-      console.log('error', error);
       console.log('error.message', error.message);
     }
   };
 
-export const authSignInUser = () => async (dispatch, getState) => {};
+export const authSignInUser =
+  ({ email, password }) =>
+  async dispatch => {
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, password);
+      console.log('user.user::', user.user);
+    } catch (error) {
+      console.log('error.message', error.message);
+    }
+  };
+
+// export const authSignInUser = () => async (dispatch, getState) => {};
 
 export const authSignOutUser = () => async (dispatch, getState) => {};
