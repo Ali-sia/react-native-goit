b8e18f72b4eeac658c2f329d9283c8d7ea75a29c
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-31 23:31:39 +0300

    create upload photo to firebase func

---
+++ src/helpers/uploadPhoto.jsx
@@ -0,0 +1,20 @@
+import { storage, app } from '../firebase/config';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { v4 as uuidv4 } from 'uuid';
+
+export default async function uploadPhotoToServer(uploadedImg, folder) {
+  try {
+    const res = await fetch(uploadedImg);
+    const file = await res.blob();
+    const uniquePostId = uuidv4();
+
+    const storageRef = ref(storage, `${folder}/${uniquePostId}`);
+    await uploadBytes(storageRef, file);
+    // get url
+    const postImageUrl = await getDownloadURL(storageRef);
+    console.log('---> ~ uploadPhotoToServer ~ postImageUrl:', postImageUrl);
+    return postImageUrl;
+  } catch (error) {
+    console.log('uploadPhotoToServer::', error.message);
+  }
+}
