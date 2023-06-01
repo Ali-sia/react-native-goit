cc613d12d46f06599fac36cd9c93499c9315c604
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-06-01 22:21:36 +0300

    Update CommentScreen.js

--- src/screens/mainScreen/CommentScreen.js
+++ src/screens/mainScreen/CommentScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import {
   StyleSheet,
@@ -12,37 +12,85 @@ import {
   Keyboard,
   TouchableWithoutFeedback,
 } from 'react-native';
-// import { useNavigation, useRoute } from '@react-navigation/native';
-import postExample2 from '../../../assets/images/postExample2.png';
+import { useRoute } from '@react-navigation/native';
+
+import { useSelector } from 'react-redux';
+import { getUser } from '../../redux/auth/authSelector';
+import { dbFirestore } from '../../firebase/config';
+import { collection, addDoc, doc, onSnapshot } from 'firebase/firestore';
 
 import CommentList from '../../components/Comment/CommentList';
 import ArrowUpIcon from '../../icons/arrowUp';
 
-export default function Comment() {
-  // const {
-  //   params: { postImg, comments },
-  // } = useRoute();
+export default function Comment({ route }) {
+  const {
+    params: {
+      imgUri,
+      // comments,
+      postId,
+    },
+  } = useRoute();
+
+  const { userId, nickName } = useSelector(getUser);
+  const [newComment, setNewComment] = useState('');
+  const [allComments, setAllComments] = useState(null);
+
+  const getAllComments = async () => {
+    try {
+      const docRef = doc(dbFirestore, 'posts', postId);
+
+      onSnapshot(collection(docRef, 'comments'), data => {
+        const comments = data.docs.map(doc => ({
+          ...doc.data(),
+        }));
+        setAllComments(comments);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllComments();
+  }, []);
+
   const [isShowKeyboard, setIsShowKeyboard] = useState(false);
   function handleCloseKeyboard() {
     setIsShowKeyboard(true);
     Keyboard.dismiss();
   }
 
+  async function sendComment() {
+    const docRef = doc(dbFirestore, 'posts', postId);
+    await addDoc(collection(docRef, 'comments'), {
+      newComment,
+      userId,
+      nickName,
+      postedDate: new Date(),
+    });
+
+    setNewComment('');
+  }
+
   return (
     <TouchableWithoutFeedback onPress={() => handleCloseKeyboard()}>
       <View style={styles.container}>
-        <Image style={styles.image} source={postExample2}></Image>
-        <CommentList />
+        <View style={styles.imageHolder}>
+          <Image style={styles.image} source={{ uri: imgUri }} />
+        </View>
+        <CommentList comments={allComments} />
         <View style={styles.containerFooter}>
           <View>
             <TextInput
               placeholder="Коментувати..."
               placeholderTextColor="#BDBDBD"
               style={styles.commentInput}
+              onChangeText={text => setNewComment(text)}
+              value={newComment}
             />
             <TouchableOpacity
               style={styles.iconWrapper}
-              onPress={() => alert('send comment')}
+              onPress={() => sendComment()}
               activeOpacity={0.8}
             >
               <ArrowUpIcon stroke={'#ffffff'} />
@@ -61,11 +109,21 @@ const styles = StyleSheet.create({
     paddingVertical: 32,
     paddingHorizontal: 16,
   },
-  image: {
-    borderRadius: 8,
+  imageHolder: {
+    height: 240,
     width: '100%',
+
+    backgroundColor: '#f0f',
+    borderRadius: 10,
+    overflow: 'hidden',
+
     marginBottom: 32,
   },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+
   containerFooter: {
     width: '100%',
     backgroundColor: '#FFFFFF',
