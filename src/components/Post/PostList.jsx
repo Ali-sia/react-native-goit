45e20e76fe65d11ebe9d3d588a341b0471312a97
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-06-01 16:01:28 +0300

    Update PostList.jsx

--- src/components/Post/PostList.jsx
+++ src/components/Post/PostList.jsx
@@ -1,63 +1,27 @@
 import React from 'react';
-import { StyleSheet, View, SafeAreaView, FlatList } from 'react-native';
+import { StyleSheet, View, SafeAreaView, FlatList, Text } from 'react-native';
 
 import Post from './Post';
 
-import postExample1 from '../../../assets/images/postExample1.png';
-import postExample2 from '../../../assets/images/postExample2.png';
-import postExample3 from '../../../assets/images/postExample3.png';
-
-const commentsFake = [
-  { author: 'ffff', text: 'super photo' },
-  { author: 'aaaa', text: 'perfecto' },
-  { author: 'rrrr', text: '!!!!! incredible!!!!!!!!!!!' },
-];
-const likesFake = 0;
-// const postsFake = [
-//   {
-//     postId: '1',
-//     postImg: postExample1,
-//     postName: 'Ліс',
-//     comments: commentsFake,
-//     likes: likesFake,
-//     postLocation: 'Ukraine',
-//   },
-//   {
-//     postId: '2',
-//     postImg: postExample2,
-//     postName: 'Лxfgxfvxvxіс',
-//     comments: commentsFake,
-//     likes: likesFake,
-//     postLocation: 'Ukraine',
-//   },
-//   {
-//     postId: '3',
-//     postImg: postExample3,
-//     postName: 'fffffffffffffffffff',
-//     comments: commentsFake,
-//     likes: likesFake,
-//     postLocation: 'Italy',
-//   },
-// ];
-
 export default function PostList({ posts }) {
+  console.log('---> ~ PostList ~ posts:', posts);
+
+  // if (posts.length === 0) {
+  //   console.log("post doesn't download yet");
+  //   return null;
+  // }
   return (
     <View style={styles.container}>
       <SafeAreaView style={styles.safeArea}>
-        <FlatList
-          data={posts}
-          renderItem={({ item }) => (
-            <Post
-              post={item}
-              // postImg={item.postImg}
-              // postName={item.postName}
-              // comments={item.comments}
-              // likes={item.likes}
-              // postLocation={item.postLocation}
-            />
-          )}
-          keyExtractor={item => item.postId}
-        />
+        {posts && (
+          <FlatList
+            data={posts}
+            renderItem={({ item }) => <Post post={item} />}
+            keyExtractor={(item, index) => {
+              index.toString();
+            }}
+          />
+        )}
       </SafeAreaView>
     </View>
   );
