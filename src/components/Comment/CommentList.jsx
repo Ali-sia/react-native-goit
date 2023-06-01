4533be3663d1c151c3a1c84e9152c53e79c93593
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-06-01 22:21:34 +0300

    Update CommentList.jsx

--- src/components/Comment/CommentList.jsx
+++ src/components/Comment/CommentList.jsx
@@ -11,38 +11,19 @@ import {
 
 import Comment from './Comment';
 
-const commentsFake = [
-  {
-    commentId: '4',
-    author: 'ffff',
-    text: 'super photo',
-    createAt: 'Mon Jun 9 2020 08:40:00 GMT+0000',
-  },
-  {
-    commentId: '5',
-    author: 'aaaa',
-    text: 'perfecto',
-    createAt: 'Mon Jun 9 2020 08:40:00 GMT+0000',
-  },
-  {
-    commentId: '6',
-    author: 'rrrr',
-    text: '!!!!! incredible!!!!!!!!!!!',
-    createAt: 'Mon Jun 9 2020 08:40:00 GMT+0000',
-  },
-];
-
-export default function CommentList() {
+export default function CommentList({ comments }) {
+  console.log('---> ~ CommentList ~ comments:', comments);
   return (
     <View style={styles.container}>
       <SafeAreaView style={styles.safeArea}>
         <FlatList
-          data={commentsFake}
+          data={comments}
           renderItem={({ item }) => (
             <Comment
-              author={item.author}
-              text={item.text}
-              data={item.createAt}
+              authorAvatar={item.userAvatar}
+              authorId={item.userId}
+              text={item.newComment}
+              data={item.postedDate}
             />
           )}
           keyExtractor={item => item.commentId}
