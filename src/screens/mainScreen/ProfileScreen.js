c99b09d7547d8b2f3c17b9970d6fe223f7059708
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-06-02 16:51:45 +0300

    Update ProfileScreen.js

--- src/screens/mainScreen/ProfileScreen.js
+++ src/screens/mainScreen/ProfileScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -11,8 +11,73 @@ import AvatarInput from '../../components/CustomAvatarInput';
 import LogOutBtn from '../../components/LogOutBtn';
 import PostList from '../../components/Post/PostList';
 
-export default function Profile({ name, email, posts }) {
-  name = 'Natali Romanova';
+import { getUser } from '../../redux/auth/authSelector';
+import { useSelector, useDispatch } from 'react-redux';
+// import { updateUserProfile } from '../../redux/auth/authSlice';
+
+// import uploadPhotoToServer from '../../helpers/uploadPhoto';
+
+import { dbFirestore } from '../../firebase/config';
+import { collection, onSnapshot, where, query } from 'firebase/firestore';
+
+export default function Profile() {
+  // const dispatch = useDispatch();
+  const { userId, nickName, userAvatar } = useSelector(getUser);
+  const [userPosts, setUserPosts] = useState([]);
+  const [commentsCount, setCommentsCount] = useState({});
+  // const [changedAvatar, setChangedAvatar] = useState('');
+
+  const getCommentsCount = async postId => {
+    try {
+      const commentsRef = collection(dbFirestore, `posts/${postId}/comments`);
+      const queryRef = query(commentsRef);
+      const unsubscribe = onSnapshot(queryRef, querySnapshot => {
+        const commentsCount = querySnapshot.docs.length;
+        setCommentsCount(prev => ({ ...prev, [postId]: commentsCount }));
+      });
+      return () => unsubscribe();
+    } catch (error) {
+      console.log(error);
+      setCommentsCount(prev => ({ ...prev, [postId]: 0 }));
+    }
+  };
+  async function getUserPosts() {
+    try {
+      const userPostsRef = collection(dbFirestore, 'posts');
+      const queryRef = query(userPostsRef, where('userId', '==', userId));
+      const unsubscribe = onSnapshot(queryRef, querySnapshot => {
+        const userPosts = querySnapshot.docs.map(doc => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setUserPosts(userPosts);
+
+        if (userPosts && userPosts.length > 0) {
+          userPosts.forEach(post => {
+            getCommentsCount(post.id.toString());
+          });
+        }
+      });
+      return () => unsubscribe();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getUserPosts();
+  }, []);
+
+  // useEffect(() => {
+  //   updateProfilePhoto(changedAvatar);
+  // }, [changedAvatar]);
+
+  // async function updateProfilePhoto(photo) {
+  //   const userAvatar = await uploadPhotoToServer(photo);
+  //   console.log('---> ~ updateProfilePhoto ~ userAvatar:', userAvatar);
+  //   dispatch(updateUserProfile(userAvatar));
+  // }
+
   return (
     <View style={styles.containerUnder}>
       <ImageBackground
@@ -22,14 +87,14 @@ export default function Profile({ name, email, posts }) {
         <View style={styles.mainContainer}>
           <View style={styles.container}>
             {/* avatar input */}
-            <AvatarInput />
+            <AvatarInput avatar={userAvatar} />
             <View style={styles.logOutBtn}>
               <LogOutBtn />
             </View>
 
-            <Text style={styles.title}>{name}</Text>
+            <Text style={styles.title}>{nickName}</Text>
             <View style={styles.containerPosts}>
-              <PostList />
+              <PostList posts={userPosts} commentsCount={commentsCount} />
             </View>
           </View>
         </View>
